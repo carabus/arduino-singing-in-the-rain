aa27fe9b582c4e71afa94eb79435aaf54a2e9efb
Svetlana Raeva <svetlana.myth@gmail.com>
2015-04-07 00:47:24 +1200

    some refatoring

--- arduino-singing-in-the-rain.ino
+++ arduino-singing-in-the-rain.ino
@@ -16,47 +16,35 @@ struct note {
   float duration;
 }; 
 
-struct note melody1[] = {
+struct note melody[] = {
   {NOTE_D4, 0.5},
   {NOTE_D4, 0.25},
   {NOTE_D4, 0.5}
 };
 
-// notes in the melody:
-int melody[] = {
-  NOTE_D4, NOTE_D4, NOTE_B3, NOTE_A3, NOTE_G3, NOTE_E3, NOTE_E3, NOTE_D3, NOTE_G3, 
-  NOTE_G3, NOTE_G3, NOTE_A3, NOTE_B3, NOTE_D4, NOTE_D4, NOTE_D3, NOTE_E3, NOTE_G3, NOTE_G3,
-  NOTE_A3, NOTE_B3, NOTE_D4, NOTE_B3, NOTE_D4, NOTE_D4, NOTE_D4, NOTE_B3, NOTE_A3,
-  NOTE_E3, NOTE_E3
-};
-
-// note durations: 4 = quarter note, 8 = eighth note, etc.:
-float noteDurations[] = {
-  2, 8, 8, 8, 8, 2, 4, 4, 2, 8, 8, 8, 8, 8, 2, 4, 8, 8, 2, 8, 8, 8, 2, 8, 8, 2, 8, 8, 8, 2, 4
-};
-
 void setup() {
   Serial.begin(9600);
-  // iterate over the notes of the melody:
-  for (int thisNote = 0; thisNote < sizeof(melody1)/sizeof(melody1[0]); thisNote++) {
+  buzzerSound();
+}
 
-    // to calculate the note duration, take one second
-    // divided by the note type.
-    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
-    int noteDuration = 1000 * melody1[thisNote].duration;
-    tone(2, melody1[thisNote].pitch, noteDuration);
-    Serial.println(melody1[thisNote].pitch);
-    Serial.println(melody1[thisNote].duration);
+void loop() {
+  // no need to repeat the melody.
+}
+
+void buzzerSound() {
+  Serial.println(sizeof(melody));
+  Serial.println(sizeof(melody[0]));
+  for (int thisNote = 0; thisNote < sizeof(melody)/sizeof(melody[0]); thisNote++) {
+    int noteDuration = 1000 * melody[thisNote].duration;
+    Serial.println(melody[thisNote].duration);
+    Serial.println(melody[thisNote].duration);    
+    tone(2, melody[thisNote].pitch, noteDuration);
 
     // to distinguish the notes, set a minimum time between them.
     // the note's duration + 30% seems to work well:
     int pauseBetweenNotes = noteDuration * 1.30;
     delay(pauseBetweenNotes);
     // stop the tone playing:
-    noTone(2);
-  }
-}
-
-void loop() {
-  // no need to repeat the melody.
+    // noTone(2);
+  }  
 }
