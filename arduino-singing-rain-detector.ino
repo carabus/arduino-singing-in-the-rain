c04dd3247414543c1497b09c4a7459e41e878603
Svetlana Raeva <svetlana.myth@gmail.com>
2015-05-07 00:04:49 +1200

    added protocol and formatting

--- arduino-singing-rain-detector.ino
+++ arduino-singing-rain-detector.ino
@@ -4,7 +4,7 @@
 const int RAIN_THRESHOLD = 700;
 const int SENSOR_PIN = A0;
 const int BUZZER_PIN = 2;
-  
+
 const int SILENCE_COMMAND = 1;
 
 enum mode {SOUND_ON, SOUND_OFF};
@@ -19,23 +19,26 @@ void loop() {
   if (Serial.available()) {
     if (Serial.read() == SILENCE_COMMAND) {
       deviceMode = SOUND_OFF;
-    } 
+    }
   }
 
-  int sensorReading = analogRead(SENSOR_PIN);
-  Serial.println(sensorReading);
+  int sensorReading = 1023;//analogRead(SENSOR_PIN);
 
-  if (sensorReading < RAIN_THRESHOLD) {
+  bool isRaining = sensorReading < RAIN_THRESHOLD;
+
+  sendMessage(isRaining, sensorReading);
+
+  if (isRaining) {
     if (deviceMode != SOUND_OFF) {
       //Serial.println("I'm singing in the rain");
-      buzzerSound();
-    }   
+      //buzzerSound();
+    }
   }
   else {
     deviceMode = SOUND_ON;
   }
 
-  delay(1000*20); // delay between reads = 1 minute
+  delay(1000 * 20); // delay between reads = 1 minute
 }
 
 void buzzerSound() {
@@ -49,3 +52,18 @@ void buzzerSound() {
     delay(pauseBetweenNotes);
   }
 }
+
+/*
+Message format:
+5 bytes
+1 - beginning of the message
+2 - isRaining
+3 - buzzerMode
+4,5 - sensorReading
+*/
+
+void sendMessage(bool isRaining, int sensorReading) {
+  byte message[] = {0xff, isRaining ? 1 : 0, deviceMode == SOUND_ON ? 1 : 0, lowByte(sensorReading), highByte(sensorReading)};
+  Serial.write(message, 5);
+}
+
